c1d3dd4f36a9f7511c728918efe02052952a4eec
sgarcete <santiago.garcete@mercadolibre.com>
2021-10-14 12:36:28 -0300

    Added spring security config to ignore token validation in user creation

--- src/main/java/com/tenpo/challenge/TenpoChallengeApplication.java
+++ src/main/java/com/tenpo/challenge/TenpoChallengeApplication.java
@@ -1,12 +1,50 @@
 package com.tenpo.challenge;
 
+import com.tenpo.challenge.filter.TokenValidatorFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class TenpoChallengeApplication {
 
+    @Autowired
+    private Environment environment;
+
     public static void main(String[] args) {
         SpringApplication.run(TenpoChallengeApplication.class, args);
     }
+
+    @EnableWebSecurity
+    @Configuration
+    class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+        //TODO: Security configs should not be disabled in tests and this conditional should not be here
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            if(Arrays.stream(environment.getActiveProfiles()).anyMatch(
+                    env -> (env.equalsIgnoreCase("test")))) {
+                http.csrf().disable()
+                        .addFilterAfter(new TokenValidatorFilter(), UsernamePasswordAuthenticationFilter.class)
+                        .authorizeRequests()
+                        .antMatchers(HttpMethod.POST, "/users").permitAll()
+                        .anyRequest().authenticated();
+            } else {
+                http.csrf().disable()
+                        .addFilterAfter(new TokenValidatorFilter(), UsernamePasswordAuthenticationFilter.class)
+                        .authorizeRequests()
+                        .antMatchers(HttpMethod.POST, "/users").permitAll()
+                        .anyRequest().authenticated();
+            }
+        }
+    }
+
 }
